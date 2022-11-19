161278c13cc0234bf38275088afcde9f441bea70
SquidwardOV <115181787+SquidwardOV@users.noreply.github.com>
2022-11-19 17:50:27 +0300

    Create doc9numb4.cpp

---
+++ doc9numb4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+    string s;
+
+    cin >> s;
+    for (int i = 0; i < s.length(); ++i){
+        if (s[i]=='+' && s[i+1]=='+'){
+            s[i]='.';
+        }
+    }
+    for (int i = 0; i < s.length(); ++i){
+        if (s[i]=='.'){
+        }
+        else {
+            cout << s[i];
+        }
+    }
+    
+    
+
+}
